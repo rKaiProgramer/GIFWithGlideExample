fd9ba265a907a3dc63bb600efd7074fcaf0db9f1
Kai <S364492@yo-woo.com>
2022-09-02 13:01:51 +0800

    feat: load a GIF url with Glide.

--- app/src/main/java/com/kai/stackoverflow/gifExample/MainActivity.kt
+++ app/src/main/java/com/kai/stackoverflow/gifExample/MainActivity.kt
@@ -2,11 +2,23 @@ package com.kai.stackoverflow.gifExample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kai.stackoverflow.gifloading.R
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.imageView)
+        loadImage()
+    }
+
+    private fun loadImage() {
+        val url = "https://media.giphy.com/media/3o7527pa7qs9kCG78A/giphy.gif"
+        Glide.with(this)
+            .load(url)
+            .into(imageView)
+
     }
 }
